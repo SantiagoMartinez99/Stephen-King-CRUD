01a859c32bacc622e3510acc3e5676ef6036df31
David <sm307560@gmail.com>
2024-06-11 18:00:35 -0500

    Initial project setup and package installation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-5xl text-orange-500">Title</h1>
+    </>
+  );
+}
+
+export default App;
